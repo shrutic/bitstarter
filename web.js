7df69369da6d1f7ae0fda5cce5cc06367ff08acb
Ubuntu <ubuntu@ip-172-31-23-30.us-west-2.compute.internal>
2013-07-10 05:22:23 +0000

    trying out api

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-   var call = Buffer.toString(fs.readFileSync('./index.html'))  
+   var call = (fs.readFileSync('./index.html')).toString();  
    response.send(call);
 });
 
